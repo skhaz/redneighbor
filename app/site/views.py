8ce1b9eb866bcac4eed09e927c8f25739cbdb80f
Rodrigo Delduca <rodrigodelduca@gmail.com>
2016-11-18 21:07:27 -0200

    Fix favicon path

--- app/site/views.py
+++ app/site/views.py
@@ -7,3 +7,13 @@ site_blueprint = Blueprint('site', __name__, template_folder='templates')
 @site_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+
+@site_blueprint.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+
+@site_blueprint.route('/nudes/<string:key>')
+def nude():
+    return render_template('upload.html')
